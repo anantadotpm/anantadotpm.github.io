191b3f8fea9223c4756b27bcd2343a1eab37b55a
anantadotpm <info@ananta.pm>
2018-06-05 12:04:15 +0200

    Add files via upload

---
+++ main-min.js
@@ -0,0 +1,2 @@
+"use strict";$(".dashboard").bgswitcher({images:["/dashboard/img/bg1.jpg","/dashboard/img/bg2.jpg","/dashboard/img/bg3.jpg","/dashboard/img/bg4.jpg","/dashboard/img/bg5.jpg"],effect:"drop"});
+//# sourceMappingURL=./main-min.js.map
